30d461da80dacdb97c33415799d77c854ba6d623
williammago <euwilliamgoncalves@gmail.com>
2021-05-13 17:58:37 -0300

    Turning Person into an abstract class

--- src/main/kotlin/one/digtalinnovation/digionebank/Person.kt
+++ src/main/kotlin/one/digtalinnovation/digionebank/Person.kt
@@ -1,10 +1,6 @@
 package one.digtalinnovation.digionebank
 
-open class Person(
-    var name: String,
-    var cpf: String,
-) {
-    open fun printInfo() {
-        println("Nome: $name\nCPF: $cpf\n")
-    }
-}
+abstract class Person(
+    val name: String,
+    val cpf: String,
+)
